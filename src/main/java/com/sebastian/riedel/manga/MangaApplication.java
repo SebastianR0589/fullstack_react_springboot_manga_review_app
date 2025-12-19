97c1e1809b6f450056e0a150198873e0b96fb499
SebastianR0589 <sebastian.riedel0589@gmail.com>
2025-12-19 14:59:18 +0100

    base code

---
+++ src/main/java/com/sebastian/riedel/manga/MangaApplication.java
@@ -0,0 +1,14 @@
+package com.sebastian.riedel.manga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MangaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangaApplication.class, args);
+	}
+
+}
